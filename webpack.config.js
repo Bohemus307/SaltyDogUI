9dca91d0ed27da2750b524239d2564e41d80f982
joshua oxner <bohemus@hotmail.com>
2020-10-14 16:02:27 -0600

    hello world beginings now to the real fun

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/src/client');
+const DIST_DIR = path.join(__dirname, '/public');
+
 module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
   module: {
     rules: [
       {
