a9cacfa810143235788a750aba29c0eb59fe631e
joshua oxner <bohemus@hotmail.com>
2020-10-20 06:35:47 -0600

    more nav bar construction and layout

--- src/client/Components/NavBar/NavBar.jsx
+++ src/client/Components/NavBar/NavBar.jsx
@@ -7,13 +7,16 @@ import classes from './NavBar.css';
 
 const NavBar = () => (
   <div className={classes.Nav_Div}>
-    <div>
+    <div className={classes.Nav_Item}>
+      <img className={classes.Nav_Image} src="/images/home.svg" alt="Home" />
       <NavLink to="/">Home</NavLink>
     </div>
-    <div>
+    <div className={classes.Nav_Item}>
+      <img className={classes.Nav_Image} src="/images/enter.svg" alt="Login" />
       <NavLink to="/login">Login</NavLink>
     </div>
-    <div>
+    <div className={classes.Nav_Item}>
+      <img className={classes.Nav_Image} src="/images/dashboard.svg" alt="Dash Board" /> 
       <NavLink to="/dashboard">Dashboard</NavLink>
     </div>
   </div>
