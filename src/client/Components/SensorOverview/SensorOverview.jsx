7b5ed607c76506d03c57348e184a96dc051d4178
joshua oxner <bohemus@hotmail.com>
2020-10-25 17:17:10 -0600

    added indiv sensor feed page

--- src/client/Components/SensorOverview/SensorOverview.jsx
+++ src/client/Components/SensorOverview/SensorOverview.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types';
 
+import classes from './SensorOverview.css';
 import Sensor from '../Sensor/Sensor.jsx';
 
 export default function SensorOverview({ type, loading, unitOfMeasure }) {
+  const [feedData, setFeedData] = useState([
+    {
+      value: 6,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+    {
+      value: 7,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+    {
+      value: 6,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+    {
+      value: 5,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+    {
+      value: 6,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+    {
+      value: 7,
+      time: new Date().toISOString(),
+      UOM: unitOfMeasure,
+    },
+  ]);
   return (
-    <div>
-      <div>
+    <div className={classes.Overview}>
+      <div className={classes.Sensor_Div}>
         <Sensor type={type} loading={loading} unitOfMeasure={unitOfMeasure} />
       </div>
-      <div>
-        Sensor feed
+      <div className={classes.Sensor_Feed}>
+        {feedData.map((item, index) => (
+          <div key={index} className={classes.Data_Reading}>
+            Current Reading:
+            <span>
+              (
+              {item.value}
+              {unitOfMeasure}
+              )
+            </span>
+            <span>{item.time}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
